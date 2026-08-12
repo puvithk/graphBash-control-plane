f2ee2f01aee3e28d33b869d3cd49b859f1352c8d
puvithk <puvithkumar2004@gmail.com>
2026-08-12 22:11:11 +0530

    feat(auth):Added JWT service

---
+++ app/api/service/jwt_service.py
@@ -0,0 +1,43 @@
+from app.core.exception import InvalidCredentialsException
+import jwt
+from app.core.config import SECRET_KEY , ALGORITHM , ACCESS_TOKEN_EXPIRE_TIME
+from datetime import datetime , timedelta , timezone
+
+
+class JwtService():
+
+    """
+    JWT Service to Access the secured resources
+    """
+    def create_token(self , data : dict , expire_time : int = ACCESS_TOKEN_EXPIRE_TIME ):
+        #Make a copy of the data which will ahve sub & iat 
+        to_encode = data.copy()
+        # Get the token expire time 
+        token_expire_time =  datetime.now(timezone.utc) + timedelta(minutes=expire_time)
+
+        #Update the expiration time 
+        to_encode.update({"exp" : token_expire_time})
+        #Encode into Token and sign it 
+        encode_token = self._encode(to_encode)
+        #Return the token if valid
+        return encode_token     
+    
+
+    def verify_token(self , token : str ):
+        #Verify the token 
+        payload : dict = self._decode(token)
+        #Get the expire time 
+        expire_time = payload.get("exp")
+        #Check the expire time 
+        if expire_time < datetime.now(timezone.utc):
+            raise InvalidCredentialsException("Token is expired")
+        
+        #Return the payload
+        return payload
+
+    def _encode(self , payload : dict):
+        
+        return jwt.encode(payload , SECRET_KEY , algorithm=ALGORITHM)
+
+    def _decode(self , token : str):
+        return jwt.decode(token ,SECRET_KEY, algorithms=[ALGORITHM])
