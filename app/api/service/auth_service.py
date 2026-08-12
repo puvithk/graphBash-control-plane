6cdd10e792dc69ddd5edadabda7493cfc118dfd1
puvithk <puvithkumar2004@gmail.com>
2026-08-12 22:55:41 +0530

    feat(auth): Updated the auth_service to incoperate

--- app/api/service/auth_service.py
+++ app/api/service/auth_service.py
@@ -1,10 +1,13 @@
 
+from app.api.service.jwt_service import JwtService
 from app.core.exception import InvalidCredentialsException
 from app.core.exception import ValueNotFoundException
 from app.api.dto.user_dto import UserSignInRequest, UserSignUpRequest, UserLoginResponse
 from app.api.service.user_service import UserService
 from ..utils.password import PasswordUtils
 from ..schemas.user import User
+jwt_service =  JwtService()
+
 class AuthService:
     def __init__(self , session):
         self.session = session
@@ -19,7 +22,12 @@ class AuthService:
         if not PasswordUtils().verify(user_request.user_password , db_user.user_hash):
             raise InvalidCredentialsException("Email or password is wrong")
 
-        return UserLoginResponse(token="dummy_token")
+
+        token = jwt_service.create_token(
+            {"email" : db_user.user_email}
+            )
+
+        return UserLoginResponse(token=token)
 
 
 
