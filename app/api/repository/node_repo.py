583b7326753a6f3072018483b16267405c5793d4
puvithk <puvithkumar2004@gmail.com>
2026-08-23 21:18:54 +0530

    feat(node) : Added token get delete and create node credentials

--- app/api/repository/node_repo.py
+++ app/api/repository/node_repo.py
@@ -1,3 +1,4 @@
+from app.api.schemas.node import NodeCredential
 from app.api.schemas.node import NodeRegisterDetails
 from app.api.dto.node_dto import NodeRegistrationToken
 from sqlalchemy import select
@@ -35,3 +36,23 @@ class NodeRepository():
         self.session.refresh(node_registration_token)
 
         return node_registration_token
+
+
+    def get_node_registration_token(self , token : str) -> NodeRegisterDetails | None:
+        statement = select(NodeRegisterDetails).where(
+            NodeRegisterDetails.token ==  token  ,
+        )
+        return self.session.execute(statement).scalar_one_or_none()
+
+    def delete_node_registration_token(self , token : str , owner_id : int):
+        self.session.execute(select(NodeRegisterDetails).where(
+            NodeRegisterDetails.token ==  token  ,
+            NodeRegisterDetails.owner_id == owner_id
+        ))
+        self.session.commit()
+
+    def create_node_credentials(self , node_credentials : NodeCredential):
+        self.session.add(node_credentials)
+        self.session.commit()
+        self.session.refresh(node_credentials)
+        return node_credentials
