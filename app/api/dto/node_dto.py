2e663029058472661a8b938ca60c5cfd32036334
puvithk <puvithkumar2004@gmail.com>
2026-08-23 21:15:57 +0530

    feat(node): add NodeRegistrationRequestDTO

--- app/api/dto/node_dto.py
+++ app/api/dto/node_dto.py
@@ -5,7 +5,31 @@ from pydantic import BaseModel , Field
 from datetime import datetime
 class NodeRequestDTO(BaseModel):
 
-    node_id : str = Field(default=None)
+
+
+    hostname : str = Field(default=None , index=True)
+
+    node_type : str = Field(default=None , index=True)
+
+    node_ip : str = Field( description="Node IP")
+
+    node_port : int = Field( description="Node Port")
+
+    node_os : str = Field( description="Node OS")
+
+    node_description : str = Field(description="Node description")
+
+    node_metadata : dict = Field(default=None , index=True)
+
+    node_status : NodeStatus = Field(default=None , index=True)
+
+class NodeRegistrationRequestDTO(BaseModel):
+    node_id : str = Field(description="Node ID")
+
+
+
+
+
     
     
 class NodeRegistrationToken(BaseModel):
@@ -27,3 +51,6 @@ class NodeRegistrationResponse(BaseModel):
 class NodeRegistrationTokenRequest(BaseModel):
 
     token : str = Field(description="Token")
+
+
+
