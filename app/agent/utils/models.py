d3a1713b294c522377a5499b448e870785229485
puvithk <puvithkumar2004@gmail.com>
2026-08-31 23:38:10 +0530

    feat(agent): implement ChatLLM wrapper with retry logic and config integration

--- app/agent/utils/models.py
+++ app/agent/utils/models.py
@@ -1,3 +1,6 @@
+from sqlalchemy.dialects.postgresql import Any
+from random import randrange
+from app.core import config
 from langchain_groq import ChatGroq
 import os 
 from dotenv import load_dotenv
@@ -13,8 +16,39 @@ class GroqLLM():
             temperature = 0.7,
             top_p = 0.9,
             stream = True,
-            api_key=os.getenv("GROQ_API_KEY"),
+            api_key=config.GROQ_API_KEY,
         )
 
+    def model_provider(self ):
+        return "groq"
 
 
+
+class ChatLLM():
+    def __init__(self , llm_provider = "groq" , llm_model = "llama-3.1-8b-instant"):
+        self.llm_provider = llm_provider
+        self.llm_model = llm_model
+        if self.llm_provider == "groq":
+            self.chat_model = GroqLLM(llm_model)
+        else:
+            raise ValueError("LLM Provider is not supported")
+    
+
+    def invoke(self , prompt : str ):
+        # Try max for 3 turns 
+        for _ in range(config.MAX_LLM_TRIES):
+            try:
+                response = self.chat_model.invoke(prompt)
+                return response
+            except Exception as e:
+                raise ValueError("LLM Error " + str(e))
+
+    def invoke_with_structured_output(self , prompt : str , output_structure ):
+        llm = self.chat_model.with_structured_output(output_structure)
+        # Try max for 3 turns 
+        for _ in range(config.MAX_LLM_TRIES):
+            try:
+                response = llm.invoke(prompt)
+                return response.json_dump()
+            except Exception as e:
+                raise ValueError("LLM Error " + str(e))
--- app/core/config.py
+++ app/core/config.py
@@ -11,3 +11,6 @@ ACCESS_TOKEN_EXPIRE_TIME = int(os.getenv("ACCESS_TOKEN_EXPIRE_TIME" , 30))
 
 DATABASE_URL = os.getenv("DATABASE_URL" ,"localhost:3306/nodebash" )
 
+GROQ_API_KEY = os.getenv("GROQ_API_KEY")
+
+MAX_LLM_TRIES = int(os.getenv("MAX_LLM_TRIES" , 3))
